a97796bd7eaa5a5a43fb354d7e6647100f337d09
riot50090-svg <riot50090@gmail.com>
2026-09-26 10:41:34 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteName: "ScrimForge",
+
+  registration: {
+    paymentMethod: "WhatsApp Business",
+    adminControlsFee: true,
+    adminControlsTime: true,
+    playerCanChangeFee: false,
+    playerCanChangeTime: false
+  },
+
+  scrims: {
+    defaultMaxTeams: 12,
+    defaultMatches: 6
+  },
+
+  leaderboard: {
+    liveRefreshSeconds: 5
+  }
+};
